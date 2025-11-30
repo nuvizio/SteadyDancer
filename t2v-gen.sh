3fa80fa66381628d0618a31800f1fc1d495c334d
YuechuanLi <ycli.mars@gmail.com>
2025-11-30 00:49:21 -0800

    fix

--- t2v-gen.sh
+++ t2v-gen.sh
@@ -1,7 +1,10 @@
 uv run python generate_dancer.py \
-    --task t2v-14B \
-    --size 1280*720 \
+    --task i2v-14B \
+    --size 832*480 \
     --ckpt_dir checkpoints/Wan2.1-T2V-14B \
     --prompt "Two anthropomorphic cats in comfy clothes cooking a romantic dinner, high quality, 4k, photorealistic" \
-    --save_file test_t2v_firstframe_dance_video.mp4
+    --image data/images/00001.png \
+    --cond_pos_folder preprocess/output/video00001_img00001/example/positive \
+    --cond_neg_folder preprocess/output/video00001_img00001/example/negative \
+    --save_file test_i2v_dance_video.mp4
 
